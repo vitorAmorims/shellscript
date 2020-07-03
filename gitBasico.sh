fbe5e2f998730c82ebf4bfb425635b0804c77298
Vitor Amorims <67655677+vitorAmorims@users.noreply.github.com>
2020-07-03 14:26:42 -0300

    Update gitBasico.sh

--- gitBasico.sh
+++ gitBasico.sh
@@ -1,5 +1,8 @@
  !#/bin/bash
 clear;
-echo "comando git status: analisa possíveis alterações."
+echo "Primeiro comando: git status: analisa possíveis alterações no diretório."
+echo "Segundo comando: git add nomeDoArquivo, para adicionar o arquivo no git"
+echo "Terceiro comando: git commit - m nomeDoarquivo, estou realizando commit no git"
+echo "Quarto comando: git push - Para enviar o commit ao GitHub"
 
 
